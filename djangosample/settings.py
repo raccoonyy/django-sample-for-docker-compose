f950bb95968cf1d9a1b31bf87be90de699a80d32
raccoony <raccoonyy@gmail.com>
2017-03-10 00:43:34 +0900

    데이터베이스 비밀번호 기본 값 변경

--- djangosample/settings.py
+++ djangosample/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DJANGO_DB_NAME', 'djangosample'),
         'USER': os.environ.get('DJANGO_DB_USERNAME', 'sampleuser'),
-        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', 'password'),
+        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', 'samplesecret'),
         'HOST': os.environ.get('DJANGO_DB_HOST', 'localhost'),
         'PORT': os.environ.get('DJANGO_DB_PORT', '5432'),
     }
